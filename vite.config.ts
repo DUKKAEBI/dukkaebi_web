ee35dfd424dafe9b61709665ed3b7faa71f57115
qpaseo <qazcgik@gmail.com>
2025-11-20 20:24:42 +0900

    refactor :: 프록시 내부 mode에 타입추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
   // 환경 변수 로드
   const env = loadEnv(mode, process.cwd(), "");
 
