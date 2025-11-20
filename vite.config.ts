420edc8f775f5f2a45c245baf2d443993ed36093
qpaseo <qazcgik@gmail.com>
2025-11-20 20:37:30 +0900

    refactor :: 프록시 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-export default ({ mode }: { mode: string }) => {
-  // 환경 변수 로드
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-  });
-};
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
